9b9a2f2eddcfdbaaa43441057565715788b8862f
study-withme <tea_meen1@daum.net>
2026-03-12 11:37:09 +0900

    feat: 게시판 및 댓글 API 기본 구조 구현

---
+++ src/main/java/com/back/domain/post/comment/controller/ApiV1CommentController.java
@@ -0,0 +1,96 @@
+package com.back.domain.post.comment.controller;
+
+import com.back.domain.post.comment.dto.CommentDto;
+import com.back.domain.post.comment.entity.Comment;
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.repository.PostRepository;
+import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts/{postId}/comments")
+public class ApiV1CommentController {
+
+    private final PostService postService;
+    private final PostRepository postRepository;
+
+    @GetMapping
+    public List<CommentDto> list(
+            @PathVariable int postId
+    ) {
+        Post post = postService.findById(postId).get();
+        List<Comment> comments = post.getComments();
+
+        List<CommentDto> commentDtoList = comments.stream()
+                .map(CommentDto::new)
+                .toList();
+
+        return commentDtoList;
+    }
+
+    @GetMapping("/{commentId}")
+    public CommentDto detail(@PathVariable int postId, @PathVariable int commentId) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
+
+        return new CommentDto(comment);
+    }
+
+
+    record CommentWriteReqBody(
+            @NotBlank(message = "02-content-내용은 필수입니다.")
+            @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상 100자 이하로 입력해주세요.")
+            String content
+    ) {
+    }
+
+    record CommentWriteResBody(
+            CommentDto commentDto
+    ) {
+    }
+
+    @PostMapping
+    @Transactional
+    public RsData<CommentWriteResBody> write(
+            @PathVariable int postId,
+            @RequestBody @Valid CommentWriteReqBody reqBody
+    ) {
+
+        Post post = postService.findById(postId).get();
+        Comment comment = post.addComment(reqBody.content);
+
+        return new RsData<>(
+                "%d번 댓글이 성공적으로 작성되었습니다.".formatted(comment.getId()),
+                "201-1",
+                new CommentWriteResBody(
+                        new CommentDto(comment)
+                )
+        );
+    }
+
+    @DeleteMapping("/{commentId}")
+    @Transactional
+    public RsData<CommentDto> delete(
+            @PathVariable int postId,
+            @PathVariable int commentId
+    ) {
+        Post post = postService.findById(postId).get();
+        Comment comment = post.findCommentById(commentId).get();
+        post.deleteComment(commentId);
+
+        return new RsData<>(
+                "%d번 댓글이 삭제되었습니다.".formatted(commentId),
+                "204-1",
+                new CommentDto(comment)
+        );
+    }
+}
---
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,121 @@
+package com.back.domain.post.post.controller;
+
+import com.back.domain.post.post.dto.PostDto;
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class ApiV1PostController {
+
+    private final PostService postService;
+    private final ParentAwareNamingStrategy parentAwareNamingStrategy;
+
+    @GetMapping
+    public List<PostDto> list() {
+        List<Post> result = postService.findAll();
+
+        List<PostDto> postDtoList = result.stream()
+                .map(PostDto::new)
+                .toList();
+
+        return postDtoList;
+    }
+
+    @GetMapping("/{id}")
+    public PostDto detail(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+        return new PostDto(post);
+    }
+
+    record PostWriteReqBody(
+            @Size(min = 2, max = 10, message = "03-title-제목은 2자 이상 10자 이하로 입력해주세요.")
+            @NotBlank(message = "01-title-제목은 필수입니다.")
+            String title,
+
+            @NotBlank(message = "02-content-내용은 필수입니다.")
+            @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상 100자 이하로 입력해주세요.")
+            String content
+    ) {
+    }
+
+    record PostWriteResBody(
+            PostDto postDto,
+            long postsCount
+    ) {
+    }
+
+    @PostMapping
+    public RsData<PostWriteResBody> write(@RequestBody @Valid PostWriteReqBody reqBody) {
+        Post post = postService.write(reqBody.title, reqBody.content);
+        long postsCount = postService.count();
+
+        return new RsData<>(
+                "%d번 글이 성공적으로 작성되었습니다.".formatted(post.getId()),
+                "201-1",
+                new PostWriteResBody(
+                        new PostDto(post),
+                        postsCount
+                )
+        );
+    }
+
+
+    record PostModifyReqBody(
+            @Size(min = 2, max = 10, message = "03-title-제목은 2자 이상 10자 이하로 입력해주세요.")
+            @NotBlank(message = "01-title-제목은 필수입니다.")
+            String title,
+
+            @NotBlank(message = "02-content-내용은 필수입니다.")
+            @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상 100자 이하로 입력해주세요.")
+            String content
+    ) {
+    }
+
+    record PostModifyResBody(
+            PostDto postDto
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<PostModifyResBody> modify(
+            @PathVariable int id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+
+        Post post = postService.modify(id, reqBody.title, reqBody.content);
+
+        return new RsData<>(
+                "%d번 글이 성공적으로 수정되었습니다.".formatted(post.getId()),
+                "200-1",
+                new PostModifyResBody(
+                        new PostDto(post)
+                )
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public RsData<Void> delete(
+            @PathVariable int id
+    ) {
+
+        postService.deleteById(id);
+
+        return new RsData<>(
+                "%d번 글이 삭제되었습니다.".formatted(id),
+                "200-1"
+        );
+    }
+}
